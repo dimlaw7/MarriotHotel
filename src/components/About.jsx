58472a59ced4f7c303ccfa28c540431af804a2b5
dimlaw7 <dimlaw7@gmail.com>
2023-05-26 17:59:32 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,13 @@ const About = () => {
         <div className='row flex-row-reverse'>
             <div className='col-12 col-lg-8 px-lg-5'>
                 <div className='l'>
-                    <h1 className='h3 py-2'>About Us</h1>
+                    <h1 className='h3 py-2'>About Marriott Hotel</h1>
                     <p className='lh-lg'>Feel the comfort with Deluxe Features in all our Guest Rooms, After a busy day in the business center of Victoria Island, return to your comfortable hotel rooms’ luxurious ambiance, all air conditioned with individual control, large windows, extra deep bathtub, delicious room service options and hotel services like concierge and laundry. </p>
                 </div>
                 <Assets display='d-none d-lg-block' border='col' col='col-4' />
             </div>
             <div className='col-12 col-lg-4 position-relative'>
-                <img src={mfv} className='img-fluid' alt='Marriot Full View' style={{height: '100%', objectFit: 'cover'}}/>
+                <img src={mfv} className='img-fluid' alt='Marriot Full View' style={{objectFit: 'cover'}}/>
                 <i className="bi bi-play-circle position-absolute top-50 start-50 text-white h2 translate-middle"></i>
             </div>
             <Assets display='d-lg-none' border='border-bottom' col='col-6' />
