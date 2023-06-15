cc24653483120b411f5c9a88aa0fb641f8098f6c
dimlaw7 <dimlaw7@gmail.com>
2023-06-15 11:27:37 +0100

    Customize Datepicker in Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,8 +42,10 @@ const Hero = () =>{
                 <div className='d-inline-block px-3'>
                   <Select options={rooms} onChange='' styles={customStyles} placeholder='Rooms / Suite'/>
                 </div>
-                <label htmlFor='date' className='d-inline-block px-3 border-start'>
+                <label htmlFor='date' className='position-relative d-inline-block px-3 border-start'>
                   <input type='date' name='date' id='date' />
+                  <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className="css-tj5bde-Svg"><path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
+                  </svg>
                 </label>
                 <div className='d-inline-block px-3 border-start'>
                   <Select options={guests} onChange='' styles={customStyles} placeholder='Guests'/>
